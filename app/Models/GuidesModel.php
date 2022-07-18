6e9c49543740bc63a5bc87c383d754078bf9d743
Drako <desarrollo3@aveonline.co>
2022-07-17 23:21:06 -0500

    git push

--- app/Models/GuidesModel.php
+++ app/Models/GuidesModel.php
@@ -4,8 +4,25 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
 
 class GuidesModel extends Model
 {
     use HasFactory;
+    use SoftDeletes;
+    protected $table = 'tbl_assets';
+    protected $primaryKey = 'id';
+    public $incrementing = true;
+
+    protected $fields = [
+        'id',
+        'type',
+        'name',
+        'ext',
+        'path',
+        'seed',
+        'environment',
+        'deleted_at'
+    ];
 }
