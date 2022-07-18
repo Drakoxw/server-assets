3442b78535d4d592711cb8f8154b517e37a0f577
Drako <desarrollo3@aveonline.co>
2022-07-17 22:19:41 -0500

    FEAT: pdf merge

--- app/Http/Controllers/PdfController.php
+++ app/Http/Controllers/PdfController.php
@@ -3,23 +3,52 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use iio\libmergepdf\Merger;
+use iio\libmergepdf\Pages;
 
 class PdfController extends Controller
 {
-    public function Merge()
+    public function Merge(Request $request)
     {
-        $pdf = app('dompdf.wrapper');
-        $pdf->loadHTML('<h1>Styde.net</h1>');
+        $this->validate($request, [
+            'guides' => 'required',
+        ]);
+        try {
+            $dataReq = $request->all();
+            $merger = new Merger;
 
-        return $pdf->download('mi-archivo.pdf');
-
+            foreach ($dataReq['guides'] as $value) {
+                $merger->addRaw(
+                    file_get_contents($value)
+                );
+            }
+            $fileName = isset($dataReq['name']) && !empty($dataReq['name']) ? $dataReq['name'] : time().'.pdf';
+            $bin = $merger->merge();
+            if ($dataReq['method'] == 'show') {
+                return response($bin)
+                    ->header('Content-type' , 'application/pdf');
+            } else {
+                return response([
+                    'status'  => 'ok',
+                    'name'    => $fileName,
+                    'message' => 'archivo creado',
+                    'base'    => 'data:application/pdf;base64,'.base64_encode($bin),
+                ], 200);
+            }
+        } catch (\Throwable $e) {
+            return response([
+                'status' => 'error',
+                'message' => $e->getMessage()
+            ], 400);
+        }
     }
+
     public function Download()
     {
         $pdf = app('dompdf.wrapper');
         $pdf->loadHTML('<h1>Styde.net</h1>');
         return $pdf->download('mi-archivo.pdf');
-
     }
+
 
 }
